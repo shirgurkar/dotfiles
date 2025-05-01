4435602cf6ff0aa7a90832c84528c1600e330039
Atharv Shirgurkar <atharv@localhost.localdomain>
2025-05-01 22:56:45 +0530

    changed waybar and neovim

--- .vimrc
+++ .vimrc
@@ -1,7 +1,175 @@
-set tabstop=2       " Number of visual spaces per tab
-set shiftwidth=2    " Number of spaces for each level of indentation
-set expandtab       " Convert tabs to spaces
-filetype on           " Enable filetype detection
-filetype plugin on    " Enable filetype-specific plugins
-filetype indent on    " Enable filetype-specific indentation
-syntax on             " Enable syntax highlighting
+" Basic Settings
+set number                " Enable line numbers
+set relativenumber        " Enable relative line numbers
+set mouse=a               " Enable mouse support
+set ignorecase            " Case-insensitive search
+set smartcase             " Smart case sensitivity
+set hlsearch              " Highlight search results
+set wrap                  " Enable line wrapping
+set breakindent           " Preserve indentation in wrapped lines
+set tabstop=4             " Tab width
+set shiftwidth=4          " Indent width
+set expandtab             " Use spaces instead of tabs
+set clipboard=unnamedplus " Use system clipboard
+set termguicolors         " True color support
+set updatetime=300        " Faster update time
+set signcolumn=yes        " Always show the sign column
+
+" Fix screen redrawing issues
+set ttyfast               " Indicate fast terminal connection
+set lazyredraw            " Don't redraw while executing macros
+set nolazyredraw          " Force redraw when needed (counteracts ghost effect)
+set redrawtime=10000      " Allow more time for redrawing
+set ttyscroll=3           " Faster scrolling
+
+" Force Vim to re-scan the entire screen
+autocmd VimEnter,BufEnter,FocusGained * redraw!
+
+" If using tmux, try to fix redraw issues
+if exists('$TMUX')
+  set ttymouse=xterm2
+  set notimeout
+  set ttimeout
+  set ttimeoutlen=100
+endif
+
+" Dictionary for txt files
+autocmd FileType text setlocal spell spelllang=en_us
+
+" Install Vim-Plug if not installed
+if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+" Vim-Plug configuration
+call plug#begin('~/.vim/plugged')
+
+" Auto pairs for brackets, quotes, etc.
+Plug 'jiangmiao/auto-pairs'
+let g:AutoPairsMapCR = 1       " Map <CR> to insert new indented line between pairs
+let g:AutoPairsMapSpace = 1    " Map Space to insert space between pairs
+let g:AutoPairsMapBS = 1       " Map Backspace to delete pairs
+
+" Color scheme - Catppuccin instead of Gruvbox
+Plug 'catppuccin/vim', { 'as': 'catppuccin' }
+
+" Status line
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+" Enable Powerline fonts (make sure you have them installed)
+let g:airline_powerline_fonts = 1
+" If you have issues with symbols, uncomment the line below
+" let g:airline_symbols_ascii = 1
+let g:airline_theme = 'catppuccin_macchiato'
+
+" Language Server Protocol - using CoC instead of native LSP
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+
+" Commenting plugin
+Plug 'tpope/vim-commentary'
+
+" Git integration
+Plug 'airblade/vim-gitgutter'
+Plug 'tpope/vim-fugitive'  " Git commands
+
+" Better syntax highlighting - using vim-polyglot instead of treesitter
+Plug 'sheerun/vim-polyglot'
+
+" File explorer - NERDTree instead of nvim-tree
+Plug 'preservim/nerdtree'
+Plug 'ryanoasis/vim-devicons'  " For file icons
+
+" Fuzzy finder - using CtrlP instead of telescope
+Plug 'ctrlpvim/ctrlp.vim'
+
+" Initialize plugin system
+call plug#end()
+
+" Set colorscheme
+syntax enable
+set background=dark
+colorscheme catppuccin_macchiato
+
+" CoC extensions to install (similar to the LSP servers in Neovim config)
+let g:coc_global_extensions = [
+  \ 'coc-clangd',
+  \ 'coc-pyright',
+  \ 'coc-java',
+  \ 'coc-tsserver',
+  \ 'coc-rust-analyzer',
+  \ 'coc-sh',
+  \ 'coc-html',
+  \ 'coc-css',
+  \ 'coc-eslint'
+  \ ]
+
+" CoC configuration
+" Use tab for trigger completion with characters ahead and navigate
+inoremap <silent><expr> <TAB>
+      \ pumvisible() ? "\<C-n>" :
+      \ <SID>check_back_space() ? "\<TAB>" :
+      \ coc#refresh()
+inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+function! s:check_back_space() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+" Use <c-space> to trigger completion
+inoremap <silent><expr> <c-space> coc#refresh()
+
+" Make <CR> auto-select the first completion item and notify coc.nvim to
+" format on enter
+inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+" GoTo code navigation
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gy <Plug>(coc-type-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+
+" Use K to show documentation in preview window
+nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+function! s:show_documentation()
+  if (index(['vim','help'], &filetype) >= 0)
+    execute 'h '.expand('<cword>')
+  elseif (coc#rpc#ready())
+    call CocActionAsync('doHover')
+  else
+    execute '!' . &keywordprg . " " . expand('<cword>')
+  endif
+endfunction
+
+" Symbol renaming
+nmap <leader>rn <Plug>(coc-rename)
+
+" Apply code actions
+nmap <leader>ca <Plug>(coc-codeaction)
+nmap <leader>qf <Plug>(coc-fix-current)
+
+" Navigate diagnostics
+nmap <silent> [g <Plug>(coc-diagnostic-prev)
+nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+" Format code
+xmap <leader>f <Plug>(coc-format-selected)
+nmap <leader>f <Plug>(coc-format-selected)
+
+" Key mappings for common operations
+let mapleader = ' '  " Set leader key to space
+nnoremap <leader>e :NERDTreeToggle<CR>
+nnoremap <leader>ff :CtrlP<CR>
+nnoremap <leader>fg :CtrlPLine<CR>
+nnoremap <leader>fb :CtrlPBuffer<CR>
+nnoremap <leader>fh :help 
+
+" Enhanced hover diagnostics - show the full error message
+" This is mostly handled by CoC settings
+
+" Add CoC status to airline
+let g:airline#extensions#coc#enabled = 1
---
+++ nvim/init.lua
@@ -0,0 +1,351 @@
+-- Basic Settings
+vim.opt.number = true           -- Enable line numbers
+vim.opt.relativenumber = true   -- Enable relative line numbers
+vim.opt.mouse = 'a'             -- Enable mouse support
+vim.opt.ignorecase = true       -- Case-insensitive search
+vim.opt.smartcase = true        -- Smart case sensitivity
+vim.opt.hlsearch = true         -- Highlight search results
+vim.opt.wrap = true             -- Enable line wrapping
+vim.opt.breakindent = true      -- Preserve indentation in wrapped lines
+vim.opt.tabstop = 4             -- Tab width
+vim.opt.shiftwidth = 4          -- Indent width
+vim.opt.expandtab = true        -- Use spaces instead of tabs
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
+vim.opt.termguicolors = true    -- True color support
+vim.opt.updatetime = 300        -- Faster update time
+vim.opt.signcolumn = 'yes'      -- Always show the sign column
+
+-- Dictionary for txt files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "text",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+  end
+})
+
+-- Install Packer if not installed
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Packer configuration
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Auto pairs for brackets, quotes, etc.
+  use {
+      'jiangmiao/auto-pairs',
+      config = function()
+          -- Default configuration is usually sufficient
+          -- You can add custom settings if needed:
+          vim.g.AutoPairsMapCR = 1       -- Map <CR> to insert new indented line between pairs
+          vim.g.AutoPairsMapSpace = 1    -- Map Space to insert space between pairs
+          vim.g.AutoPairsMapBS = 1       -- Map Backspace to delete pairs
+      end
+  }
+
+  -- Color scheme
+  use {
+    'catppuccin/nvim',
+    as = 'catppuccin',
+    config = function()
+      vim.cmd('colorscheme catppuccin-macchiato')
+    end
+  }
+
+  -- Airline status line
+  use {
+    'vim-airline/vim-airline',
+    requires = {'vim-airline/vim-airline-themes'},
+    config = function()
+      vim.g.airline_powerline_fonts = 1
+      vim.g.airline_theme = 'catppuccin'
+    end
+  }
+
+  -- LSP Configuration
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    }
+  }
+
+  -- Autocompletion
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'onsails/lspkind-nvim',
+    }
+  }
+
+  -- Commenting plugin
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
+  -- Git integration
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  use 'tpope/vim-fugitive'  -- Git commands
+
+  -- Treesitter for better syntax highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- File explorer
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('nvim-tree').setup{}
+    end
+  }
+
+  -- Fuzzy finder
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = 'nvim-lua/plenary.nvim'
+  }
+
+  -- Automatically set up configuration after cloning packer.nvim
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+-- LSP and completion setup
+local lspconfig = require('lspconfig')
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local opts = { noremap=true, silent=true }
+
+  -- LSP keybindings
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+-- Setup Mason and Mason-lspconfig
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'clangd',           -- C/C++
+    'pyright',          -- Python
+    'jdtls',            -- Java
+    'ts_ls',            -- JavaScript/TypeScript
+    'rust_analyzer',    -- Rust
+    'bashls',           -- Bash
+    'html',             -- HTML
+    'cssls',            -- CSS
+    'eslint',           -- JavaScript linting
+  }
+})
+
+-- Setup nvim-cmp
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local lspkind = require('lspkind')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = 50,
+    })
+  }
+})
+
+-- Setup individual LSP servers
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- C/C++
+lspconfig.clangd.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- Python
+lspconfig.pyright.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- Java
+lspconfig.jdtls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- JavaScript/TypeScript (including React, Node, Express)
+lspconfig.ts_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+}
+
+-- Rust
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- Bash
+lspconfig.bashls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- HTML
+lspconfig.html.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- CSS
+lspconfig.cssls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- ESLint
+lspconfig.eslint.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- Setup Treesitter
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    "c", "cpp", "python", "java", "javascript", "typescript",
+    "tsx", "rust", "bash", "html", "css", "json", "lua"
+  },
+  highlight = {
+    enable = true,
+  },
+}
+
+-- Key mappings for common operations
+vim.g.mapleader = ' '  -- Set leader key to space
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+
+-- Enhanced hover diagnostics - show the full error message
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+})
+
+-- Configure the hover behavior
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = "rounded",
+    width = 60,
+    focusable = false,
+  }
+)
+
+-- Ensure diagnostic popups are more informative
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = {
+      spacing = 4,
+      prefix = '●'
+    },
+    signs = true,
+    update_in_insert = false,
+  }
+)
